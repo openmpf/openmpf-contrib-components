d6d8b7853ad69b6e424e96c129aede2916c60922
brosenberg <brosenberg@mitre.org>
2017-09-25 15:20:58 -0400

    Use ceiling to calculate padding.

--- cpp/OcvPersonDetection/tracking/detector.cpp
+++ cpp/OcvPersonDetection/tracking/detector.cpp
@@ -137,13 +137,13 @@ void HogDetector::detect(const Mat& frame)
 {
 	// Padding was originally always set to Size(0, 0), but cpu_hog.detectMultiScale was causing a segmentation fault
 	// when the frame size was less than the window size. To prevent the segmentation fault,
-	// we add enough padding so that the resulting size is equal to the window size.
+	// we add enough padding so that the resulting size is greater than or equal to the window size.
 
     cv::Size sizeDiff = cpu_hog.winSize - frame.size();
     // A negative dimension means there is already enough space so that dimension gets set to 0.
     cv::Size extraSpaceNeeded(std::max(0, sizeDiff.width), std::max(0, sizeDiff.height));
-    // Padding gets applied to both sides.
-	cv::Size padding = extraSpaceNeeded / 2;
+    // Padding gets applied to both sides. Need to add %2 because division will round down for odd numbers.
+	cv::Size padding = extraSpaceNeeded / 2 + cv::Size(extraSpaceNeeded.width % 2, extraSpaceNeeded.height % 2);
 
 	cpu_hog.detectMultiScale(frame, detection, response, 0.0, Size(8,8), padding, 1.05, 2);
 }
