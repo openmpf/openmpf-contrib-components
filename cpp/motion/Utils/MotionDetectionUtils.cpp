5896623ab430b236f09f4ffc23cdee9ebbadeaa8
Carol Nowacki <cnowacki@mitre.org>
2020-02-04 13:13:17 -0500

    changed the name to AssignDetectionConfidence, moved it to the MotionUtils and added it to the streaming subsense component.

--- cpp/motion/Utils/MotionDetectionUtils.cpp
+++ cpp/motion/Utils/MotionDetectionUtils.cpp
@@ -229,3 +229,55 @@ cv::Rect Upscale(const cv::Rect &rect,
 bool operator <(const cv::Rect &r1, const cv::Rect &r2) {
     return r1.x != r2.x || r1.y != r2.y || r1.area() < r2.area();
 }
+
+
+void AssignDetectionConfidence(MPF::COMPONENT::MPFVideoTrack &track,
+                               float distance_factor,
+                               float size_factor) {
+    // Distance confidence: Detections closer to the "center" of the
+    // track have higher confidence. Determine which detection is at the center
+    // of the track, and assign it the highest confidence (1.0). The rest of the
+    // detections are assigned a confidence based on their distance
+    // from the "center", with the first and last detections in the
+    // track having confidence equal to 0. If there are an even number of
+    // detections in the track, then the two detections nearest the
+    // track center will both be given confidence equal to 1.0.
+
+    // Size confidence: Detections that have greater area in their
+    // bounding boxes have higher confidence. Each detection has a
+    // size confidence equal to the ratio of the size of its bounding
+    // box to the maximum size bounding box in the track.
+
+    int number_of_detections = track.frame_locations.size();
+    float max_size = 0;
+    for (auto entry: track.frame_locations) {
+        float entry_size = static_cast<float>(entry.second.width * entry.second.height);
+        if (entry_size > max_size) {
+            max_size = entry_size;
+        }
+    }
+
+    if (number_of_detections <= 2) {
+        for (auto &entry : track.frame_locations) {
+            MPFImageLocation &loc = entry.second;
+            loc.confidence = distance_factor + size_factor*(static_cast<float>(loc.height*loc.width)/max_size);
+        }
+    }
+    else {
+        float center = static_cast<float>(number_of_detections)/2.0;
+        int center_index = static_cast<int>(center);
+        float confidence_incr = 1.0/center_index;
+
+        auto first = track.frame_locations.begin();
+        auto last = track.frame_locations.rbegin();
+        for (int i = 0; i <= center_index; ++i) {
+            MPFImageLocation &loc1 = (first++)->second;
+            MPFImageLocation &loc2 = (last++)->second;
+            loc1.confidence = distance_factor*(i*confidence_incr) +
+                              size_factor*(static_cast<float>(loc1.height*loc1.width)/max_size);
+            loc2.confidence = distance_factor*(i*confidence_incr) +
+                              size_factor*(static_cast<float>(loc2.height*loc2.width)/max_size);
+        }
+    }
+}
+
