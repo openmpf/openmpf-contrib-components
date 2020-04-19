1cb31832ed56a69a4572fa7a4b58941cd9a7062f
Franz A. Pertl <fpertl@gmail.com>
2020-04-19 14:21:01 +0000

    feature and cost operations pulled into detections

---
+++ cpp/OcvSsdFaceDetection/DetectionLocation.cpp
@@ -0,0 +1,342 @@
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#include "DetectionLocation.h"
+
+using namespace MPF::COMPONENT;
+ 
+// Shared static members
+log4cxx::LoggerPtr                DetectionLocation::_log;
+unique_ptr<cv::dnn::Net>          DetectionLocation::_ssdNetPtr         = NULL;   ///< single shot DNN face detector network
+unique_ptr<dlib::shape_predictor> DetectionLocation::_shapePredFuncPtr  = NULL;   ///< landmark detector function pointer
+unique_ptr<cv::dnn::Net>          DetectionLocation::_openFaceNetPtr    = NULL;   ///< feature generator
+cv::Ptr<cv::face::FacemarkLBF>    DetectionLocation::_facemarkPtr;                ///< landmark detector
+
+/** **************************************************************************
+* Conveniance operator to dump MPFLocation to a stream
+*************************************************************************** */ 
+ostream& operator<< (ostream& out, const MPF::COMPONENT::DetectionLocation& d) {
+  out << "[" << (MPF::COMPONENT::MPFImageLocation)d 
+            // << " L:" << d.landmarks << " F["
+              << d.getFeature().size() << "] T["
+              << d.getThumbnail().rows << "," << d.getThumbnail().cols << "]";
+  return out;
+}
+
+/** **************************************************************************
+* Conveniance operator to dump MPF::COMPONENT::Track to a stream
+*************************************************************************** */ 
+ostream& operator<< (ostream& out, const MPF::COMPONENT::Track& t) {
+  MPF::COMPONENT::DetectionLocation d = t.back();
+  out << "("<<t.size()<<") ... [" << (MPF::COMPONENT::MPFImageLocation)d 
+              << d.getFeature().size() << "] T["
+              << d.getThumbnail().rows << "," << d.getThumbnail().cols << "]";
+  return out;
+}
+
+/** ****************************************************************************
+*  Draw polylines to visualize landmark features
+*
+* \param im        image to draw landmarks on
+* \param landmarks all landmark point only some of which will be drawn
+* \param start     start landmark point index for polyline
+* \param end       end landmark point index for polyline
+* \param isClosed  if true polyline draw a closed shape (end joined to start)
+* \param drawColor color to use for drawing
+*
+***************************************************************************** */
+void drawPolyline(cv::Mat &im, const cvPoint2fVec &landmarks,
+                  const int start, const int end, bool isClosed = false,
+                  const cv::Scalar drawColor = cv::Scalar(255, 200,0)){
+    cvPointVec points;
+    for (int i = start; i <= end; i++){
+        points.push_back(cv::Point(landmarks[i].x, landmarks[i].y));
+    }
+    cv::polylines(im, points, isClosed, drawColor, 2, 16);
+}
+
+/** ****************************************************************************
+* Visualize landmark point on image by drawing them.  If 68 landmarks are
+* available, they are drawn as polygons, otherwise just as points
+*
+* \param im        image to draw landmarks on
+* \param drawColor color to use for drawing
+*
+***************************************************************************** */
+void DetectionLocation::drawLandmarks(cv::Mat &img,
+                   const cv::Scalar drawColor = cv::Scalar(255, 200,0)) const {
+  cvPoint2fVec landmarks = getLandmarks();
+  if(landmarks.size() == 68){
+    drawPolyline(img, landmarks,  0, 16, false, drawColor);    // Jaw line
+    drawPolyline(img, landmarks, 17, 21, false, drawColor);    // Left eyebrow
+    drawPolyline(img, landmarks, 22, 26, false, drawColor);    // Right eyebrow
+    drawPolyline(img, landmarks, 27, 30, false, drawColor);    // Nose bridge
+    drawPolyline(img, landmarks, 30, 35, true,  drawColor);    // Lower nose
+    drawPolyline(img, landmarks, 36, 41, true,  drawColor);    // Left eye
+    drawPolyline(img, landmarks, 42, 47, true,  drawColor);    // Right Eye
+    drawPolyline(img, landmarks, 48, 59, true,  drawColor);    // Outer lip
+    drawPolyline(img, landmarks, 60, 67, true,  drawColor);    // Inner lip
+  }else { 
+    for(size_t i = 0; i < landmarks.size(); i++){
+      cv::circle(img,landmarks[i],3, drawColor, cv::FILLED);
+    }
+  }
+}
+
+/** **************************************************************************
+* Compute the Intersection Over Union metric for two detections comprised of 
+* the ratio of the area of the intersection of the detection rectangels
+* divided by the area of the union of the detection rectangles 
+* 
+* \param   d second detection 
+* \returns   intersection over union
+*
+*************************************************************************** */
+float DetectionLocation::iou(const DetectionLocation &d) const {
+	int ulx = max(x_left_upper         , d.x_left_upper           );
+	int uly = max(y_left_upper         , d.y_left_upper           );
+	int lrx = min(x_left_upper + width , d.x_left_upper + d.width );
+	int lry = min(y_left_upper + height, d.y_left_upper + d.height);
+
+	float inter_area = max(0, lrx - ulx + 1) * max(0, lry - uly + 1);	
+  float union_area = width * height + d.width * d.height - inter_area;
+  return inter_area / union_area;
+}
+
+/** **************************************************************************
+* Compute the temporal distance in frame counts between two detections
+*
+* \param   d second detection 
+* \returns   absolute difference in frame indecies
+*
+*************************************************************************** */
+float DetectionLocation::frameGap(const DetectionLocation &d) const {
+  if(frameIdx > d.frameIdx){
+    return frameIdx - d.frameIdx;
+  }else{
+    return d.frameIdx - frameIdx;
+  }
+}
+
+/** **************************************************************************
+* Compute euclidian center to distance center distance from normalized centers
+*
+* \param   d second detection 
+* \returns   normalized center to center distance [0 ... Sqrt(2)]
+*
+*************************************************************************** */
+float  DetectionLocation::center2CenterDist(const DetectionLocation &d) const {
+  float dx = center.x - d.center.x;
+  float dy = center.y - d.center.y;
+  return sqrt( dx*dx + dy*dy );
+}
+
+/** **************************************************************************
+* Compute feature distance (similarity) between two detection feature vectors
+*
+* \param   d second detection 
+* \returns   cos distance [0 ... 1.0]
+*
+* \note Feature vectors are expected to be of unit magnitude
+*
+*************************************************************************** */
+float DetectionLocation::featureDist(const DetectionLocation &d) const {
+  return static_cast<float>(norm(getFeature(), d.getFeature(),cv::NORM_L2));
+}
+
+
+/** **************************************************************************
+* Lazy accessor method to get/compute landmark points
+* 5-Landmark detector returns outside and inside eye corners and bottom of nose
+* 68-Landmark detector returns "standard" facial landmarks (see data/landmarks.png)
+*
+* \returns landmarks 
+* 
+*************************************************************************** */
+const cvPoint2fVec& DetectionLocation::getLandmarks() const {
+  if(_landmarks.empty()){
+    // OpenCV 68-landmark detector
+    // cvRectVec detections;                                       
+    // for(auto &l:locations){
+    //   detections.push_back(cv::Rect(l.x_left_upper,l.y_left_upper,l.width,l.height));
+    // }
+    // cvPoint2fVecVec landmarks;
+    // if(! _facemark->fit(cfg.bgrFrame, detections, landmarks)){
+    //   LOG4CXX_WARN(_log,"failed to generate facial landmarks");
+    // }
+
+    // DLib 5-landmark detector
+    dlib::cv_image<dlib::bgr_pixel> cimg(*_bgrFramePtr);
+    dlib::full_object_detection
+      shape = (*_shapePredFuncPtr)(cimg, dlib::rectangle(x_left_upper,               // left
+                                                          y_left_upper,              // top
+                                                          x_left_upper + width-1,    // right
+                                                          y_left_upper + height-1)); // bottom
+                                                      
+    for(size_t i=0; i<shape.num_parts(); i++){
+      dlib::point pt = shape.part(i);                                          //LOG4CXX_TRACE(_log, "lm[" << i << "]: " << shape.part(i));                  
+      _landmarks.push_back(cv::Point2f(pt.x(),pt.y()));   
+    }
+  }
+  return _landmarks;
+}
+
+/** **************************************************************************
+* Lazy accessor method to get/copy-create 92x92 thumbnail image for 
+* feature generation
+*
+* \returns thumbnail image of detection
+*
+*************************************************************************** */
+const cv::Mat& DetectionLocation::getThumbnail() const {
+  if(_thumbnail.empty()){
+    const int  THUMBNAIL_SIZE_X = 96;
+    const int  THUMBNAIL_SIZE_Y = 96;
+    const cv::Size THUMB_SIZE(THUMBNAIL_SIZE_X,THUMBNAIL_SIZE_Y);
+      // Landmark indices for OpenFace nn4.v1
+      // const size_t lmIdx[] = {39,42,57};
+      // const cv::Mat dst = (cv::Mat_<float>(3,2)
+      //                      << 0.36378494 * THUMBNAIL_SIZE_X, 0.17794687 * THUMBNAIL_SIZE_Y,
+      //                         0.61896320 * THUMBNAIL_SIZE_X, 0.17277813 * THUMBNAIL_SIZE_Y,
+      //                         0.50020397 * THUMBNAIL_SIZE_X, 0.75058440 * THUMBNAIL_SIZE_Y); 
+      
+      // Landmark indecies for OpenFace nn4.v2, nn4.small1.v1, nn4.small2.v1
+      //const size_t lmIdx[] = {36,45,33};  // if using 68 pt landmarks
+      const size_t lmIdx[] = {2,0,4};       // if using 5 pt landmarks
+      const cv::Mat dst =  (cv::Mat_<float>(3,2)
+                            << 0.1941570 * THUMBNAIL_SIZE_X, 0.16926692 * THUMBNAIL_SIZE_Y,
+                               0.7888591 * THUMBNAIL_SIZE_X, 0.15817115 * THUMBNAIL_SIZE_Y,
+                               0.4949509 * THUMBNAIL_SIZE_X, 0.51444140 * THUMBNAIL_SIZE_Y);
+
+      cv::Mat src = cv::Mat_<float>(3,2);
+      for(size_t r=0; r<3; r++){
+        float* rowPtr = src.ptr<float>(r);
+        rowPtr[0] = getLandmarks()[lmIdx[r]].x; 
+        rowPtr[1] = getLandmarks()[lmIdx[r]].y;      
+      }
+      cv::Mat xfrm = cv::getAffineTransform(src,dst);
+      _thumbnail = cv::Mat(THUMB_SIZE,_bgrFramePtr->type());
+      cv::warpAffine(*_bgrFramePtr,_thumbnail,cv::getAffineTransform(src,dst),
+                    THUMB_SIZE,cv::INTER_CUBIC,cv::BORDER_REPLICATE);
+
+  }
+  return _thumbnail;
+}
+
+/** **************************************************************************
+* Lazy accessor method to get/compute feature fector based on thumbnail 
+*
+* \returns unit magnitude feature vector
+* 
+*************************************************************************** */
+const cv::Mat& DetectionLocation::getFeature() const {
+  if(_feature.empty()){
+    const double inScaleFactor = 1.0 / 255.0;
+    const cv::Size blobSize(96, 96);
+    const cv::Scalar meanVal(0.0, 0.0, 0.0);  // BGR mean pixel color 
+    cv::Mat inputBlob = cv::dnn::blobFromImage(getThumbnail(), // BGR image
+                                              inScaleFactor,   // no pixel value scaline (e.g. 1.0/255.0)
+                                              blobSize,        // expected network input size: 90x90
+                                              meanVal,         // mean BGR pixel value
+                                              true,            // swap RB channels
+                                              false);          // center crop
+    _openFaceNetPtr->setInput(inputBlob);
+    _feature = _openFaceNetPtr->forward().clone();             // need to clone as mem gets reused
+  }
+  return _feature;
+}
+
+/** ****************************************************************************
+* Detect objects using SSD DNN opencv face detector network   
+* 
+* \param  cfg job configuration setting including iamge frame
+* 
+* \returns found face detections that meet size requirments.
+*
+* \note each detection hang on to a reference to the bgrFrame which
+*       should be released once no longer needed (i.e. features care computed)
+*
+*************************************************************************** */
+DetectionLocationVec DetectionLocation::createDetections(const JobConfig &cfg){
+  DetectionLocationVec detections;
+
+  const double inScaleFactor = 1.0;
+  const cv::Size blobSize(300, 300);
+  const cv::Scalar meanVal(104.0, 117.0, 124.0);  // BGR mean pixel color       
+
+  cv::Mat inputBlob = cv::dnn::blobFromImage(*cfg.bgrFramePtr,  // BGR image
+                                            inScaleFactor, // no pixel value scaline (e.g. 1.0/255.0)
+                                            blobSize,      // expected network input size: 300x300
+                                            meanVal,       // mean BGR pixel value
+                                            true,          // swap RB channels
+                                            false          // center crop
+                                            );
+  _ssdNetPtr->setInput(inputBlob,"data");
+  cv::Mat detection = _ssdNetPtr->forward("detection_out");
+  cv::Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+
+  for(int i = 0; i < detectionMat.rows; i++){
+    float conf = detectionMat.at<float>(i, 2);
+    if(conf > cfg.confThresh){
+      cv::Point2f ul(detectionMat.at<float>(i, 3),detectionMat.at<float>(i, 4));
+      cv::Point2f lr(detectionMat.at<float>(i, 5),detectionMat.at<float>(i, 6));
+      cv::Point2f wh = lr - ul;
+      int x1     = static_cast<int>(ul.x * cfg.bgrFramePtr->cols);
+      int y1     = static_cast<int>(ul.y * cfg.bgrFramePtr->rows);
+      int width  = static_cast<int>(wh.x * cfg.bgrFramePtr->cols);
+      int height = static_cast<int>(wh.y * cfg.bgrFramePtr->rows);
+
+      if(    width  >= cfg.minDetectionSize
+          && height >= cfg.minDetectionSize){
+        detections.push_back(DetectionLocation(x1, y1, width, height, conf,
+                                              (ul + lr) / 2.0,
+                                                cfg.frameIdx, cfg.bgrFramePtr));
+      }
+    }                                                                               
+  }
+  return detections;
+}
+
+/** **************************************************************************
+* Setup class shared static configurations and initialize / load shared 
+* detectors and feature generator objects.
+*************************************************************************** */
+bool DetectionLocation::Init(log4cxx::LoggerPtr log, string plugin_path){
+
+  _log = log;
+
+  // Load SSD Tensor Flow Network
+  string  tf_model_path = plugin_path + "/data/opencv_face_detector_uint8.pb";
+  string tf_config_path = plugin_path + "/data/opencv_face_detector.pbtxt";
+  string lbf_model_path = plugin_path + "/data/lbfmodel.yaml";
+  
+  //string  sp_model_path = plugin_path + "/data/shape_predictor_68_face_landmarks.dat";
+  string  sp_model_path = plugin_path + "/data/shape_predictor_5_face_landmarks.dat";
+  string  tr_model_path = plugin_path + "/data/nn4.small2.v1.t7";
+
+  string err_msg = "Failed to load models: " + tf_config_path + ", " + tf_model_path + ", " + lbf_model_path;
+
+  try{
+      // load detector net
+      _ssdNetPtr = unique_ptr<cv::dnn::Net>(&cv::dnn::readNetFromTensorflow(tf_model_path, tf_config_path));
+      
+      // load landmark finder
+      _facemarkPtr = cv::face::FacemarkLBF::create();
+      _facemarkPtr->loadModel(lbf_model_path);
+
+      _shapePredFuncPtr = unique_ptr<dlib::shape_predictor>(new dlib::shape_predictor());
+      dlib::deserialize(sp_model_path) >> *_shapePredFuncPtr;
+
+      // load feature generator
+      _openFaceNetPtr = unique_ptr<cv::dnn::Net>(&cv::dnn::readNetFromTorch(tr_model_path));
+
+  }catch(const runtime_error& re){                                           LOG4CXX_FATAL(_log, err_msg << " Runtime error: " << re.what());
+      return false;
+  }catch(const exception& ex){                                               LOG4CXX_FATAL(_log, err_msg << " Exception: " << ex.what());
+      return false;
+  }catch(...){                                                               LOG4CXX_FATAL(_log, err_msg << " Unknown failure occurred. Possible memory corruption");
+      return false;
+  } 
+}
+
--- cpp/OcvSsdFaceDetection/DetectionLocation.h
+++ cpp/OcvSsdFaceDetection/DetectionLocation.h
@@ -1,59 +1,83 @@
 #ifndef OCVSSDFACEDETECTION_DETECTIONLOCATION_H
 #define OCVSSDFACEDETECTION_DETECTIONLOCATION_H
 
+#include <log4cxx/logger.h>
+
+#include <opencv2/dnn.hpp>
+#include <opencv2/face.hpp>
+
+#include <dlib/image_processing.h>
+#include <dlib/opencv.h>
+
 #include "types.h" 
+#include "JobConfig.h"
+
+#define CALL_MEMBER_FUNC(object,ptrToMember)  ((object).*(ptrToMember))
 
 namespace MPF{
  namespace COMPONENT{
 
   using namespace std;
 
-  class DetectionLocation: public MPFImageLocation{
-
-    public:
-      using MPFImageLocation::MPFImageLocation;  // C++11 inherit all constructors
-
-      cv::Point2f  center;                       ///< bounding box center normalized to image size
-      size_t       frameIdx;                     ///< frame index of detection (for videos)
-
-      cvPoint2fVec landmarks;                    ///< vector of landmarks (e.g. eyes, nose, etc.)
-      cv::Mat      thumbnail;                    ///< 96x96 image comprising an aligned thumbnail
-      cv::Mat      feature;                      ///< DNN feature for matching-up detections
-
-      DetectionLocation(int x,int y,int width,int height,float conf,
-                        cv::Point2f center, size_t frameIdx):
-        MPFImageLocation(x,y,width,height,conf),
-        center(center),
-        frameIdx(frameIdx){};
-
-  };
+  class DetectionLocation;
 
   typedef vector<DetectionLocation> DetectionLocationVec;       ///< vector of DetectionLocations
-  typedef DetectionLocationVec      Track;                      ///< a track is an ordered list of detections
+  typedef DetectionLocationVec      Track;                      ///< track as an ordered list of detections
   typedef list<Track>               TrackList;                  ///< list of tracks
-  
-  /* **************************************************************************
-  * Conveniance operator to dump MPFLocation to a stream
-  *************************************************************************** */ 
-  ostream& operator<< (ostream& out, const DetectionLocation& d) {
-    out << "[" << (MPFImageLocation)d 
-              // << " L:" << d.landmarks << " F["
-               << d.feature.size() << "] T["
-               << d.thumbnail.rows << "," << d.thumbnail.cols << "]";
-    return out;
-  }
+  typedef float (DetectionLocation::*DetectionLocationCostFunc)(const DetectionLocation &d); ///< cost member function pointer type
 
-  ostream& operator<< (ostream& out, const Track& t) {
-    DetectionLocation d = t.back();
-    out << "("<<t.size()<<") ... [" << (MPFImageLocation)d 
-               << d.feature.size() << "] T["
-               << d.thumbnail.rows << "," << d.thumbnail.cols << "]";
-    return out;
-  }
+  class DetectionLocation: public MPFImageLocation{
+
+    public: 
+      using MPFImageLocation::MPFImageLocation;  // C++11 inherit all constructors for MPFImageLocation
+
+      const cv::Point2f  center;                 ///< bounding box center normalized to image dimensions
+      const size_t     frameIdx;                 ///< frame index frame where detection is located (for videos)
+
+      static bool Init(log4cxx::LoggerPtr log,
+                       const string       plugin_path);                     ///< setup class shared memebers
+
+      static DetectionLocationVec createDetections(const JobConfig &cfg);   ///< created detection objects from image frame
+
+      const cvPoint2fVec&  getLandmarks() const;                            ///< get landmark points for detection   
+      const cv::Mat&       getThumbnail() const;                            ///< get thumbnail image for detection
+      const cv::Mat&       getFeature()   const;                            ///< get DNN features for detection 
+
+      float               iou(const DetectionLocation &d) const;            ///< compute intersection over union
+      float          frameGap(const DetectionLocation &d) const;            ///< compute temporal frame gap
+      float center2CenterDist(const DetectionLocation &d) const;            ///< compute normalized center to center distance
+      float       featureDist(const DetectionLocation &d) const;            ///< compute deep feature similarity distance
+      
+      void drawLandmarks(cv::Mat &img, const cv::Scalar drawColor) const;   ///< draw landmark point on image
+      void releaseBGRFrame(){_bgrFramePtr.reset();}                         ///< release reference to image frame 
+
+    private:
+
+      static log4cxx::LoggerPtr                _log;                ///< shared log opbject
+      static unique_ptr<cv::dnn::Net>          _ssdNetPtr;          ///< single shot DNN face detector network
+      static cv::Ptr<cv::face::FacemarkLBF>    _facemarkPtr;        ///< landmark detector
+      static unique_ptr<dlib::shape_predictor> _shapePredFuncPtr;   ///< landmark detector
+      static unique_ptr<cv::dnn::Net>          _openFaceNetPtr;     ///< feature generator
+
+
+      shared_ptr<cv::Mat>  _bgrFramePtr;                  ///< pointer to frame associated with detection
+      mutable cvPoint2fVec _landmarks;                    ///< vector of landmarks (e.g. eyes, nose, etc.)
+      mutable cv::Mat      _thumbnail;                    ///< 96x96 image comprising an aligned thumbnail
+      mutable cv::Mat      _feature;                      ///< DNN feature for matching-up detections
+
+      DetectionLocation(int x,int y,int width,int height,float conf,
+                        cv::Point2f center, size_t frameIdx,
+                        shared_ptr<cv::Mat> bgrFramePtr):
+        MPFImageLocation(x,y,width,height,conf),
+        center(center),
+        frameIdx(frameIdx),
+        _bgrFramePtr(bgrFramePtr){};              ///< private constructor for createDetections()
+  };
+ 
+  ostream& operator<< (ostream& out, const MPF::COMPONENT::DetectionLocation& d); 
+  ostream& operator<< (ostream& out, const MPF::COMPONENT::Track& t);
 
  }
 }
-
-
 
 #endif
