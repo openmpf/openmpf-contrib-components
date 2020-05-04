05a319aab52d23ae7a53653a5ea6809ea10dfdff
Franz A. Pertl <fpertl@gmail.com>
2020-05-04 16:14:46 +0000

    updated licence headers

--- cpp/OcvSsdFaceDetection/DetectionLocation.cpp
+++ cpp/OcvSsdFaceDetection/DetectionLocation.cpp
@@ -1,3 +1,28 @@
+/******************************************************************************
+ * NOTICE                                                                     *
+ *                                                                            *
+ * This software (or technical data) was produced for the U.S. Government     *
+ * under contract, and is subject to the Rights in Data-General Clause        *
+ * 52.227-14, Alt. IV (DEC 2007).                                             *
+ *                                                                            *
+ * Copyright 2020 The MITRE Corporation. All Rights Reserved.                 *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Copyright 2020 The MITRE Corporation                                       *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
 
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -134,17 +159,6 @@ float DetectionLocation::featureDist(const DetectionLocation &d) const {
 *************************************************************************** */
 const cvPoint2fVec& DetectionLocation::getLandmarks() const {
   if(_landmarks.empty()){
-    // OpenCV 68-landmark detector
-    // cvRectVec detections;                                       
-    // for(auto &l:locations){
-    //   detections.push_back(cv::Rect(l.x_left_upper,l.y_left_upper,l.width,l.height));
-    // }
-    // cvPoint2fVecVec landmarks;
-    // if(! _facemark->fit(cfg.bgrFrame, detections, landmarks)){
-    //   LOG4CXX_WARN(_log,"failed to generate facial landmarks");
-    // }
-
-    // DLib 5-landmark detector
     try{
       dlib::cv_image<dlib::bgr_pixel> cimg(_bgrFrame);
       dlib::full_object_detection
